8a4a8849119e98e6235b175ba37edfd51e038476
sabbir10s <sabbirahmed1023@gmail.com>
2024-01-17 14:46:21 +0600

    footer section added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Components/Shared/Navbar/Navbar";
+import Footer from "../Components/Shared/Footer/Footer";
 
 const Main = () => {
   return (
     <div className="font-SourceSans">
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
